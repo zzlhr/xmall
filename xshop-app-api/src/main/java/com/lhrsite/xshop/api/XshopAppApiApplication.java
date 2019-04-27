3c6f7460e5b5f9c102d58a0e93cbef9ed7786d5e
zzlhr <2388399752@qq.com>
2019-04-27 17:13:55 +0800

    更新启动配置

--- xshop-app-api/src/main/java/com/lhrsite/xshop/api/XshopAppApiApplication.java
+++ xshop-app-api/src/main/java/com/lhrsite/xshop/api/XshopAppApiApplication.java
@@ -1,9 +1,17 @@
 package com.lhrsite.xshop.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.lhrsite.xshop.service",
+        "com.lhrsite.xshop.mapper", "com.lhrsite.xshop.repository", "com.lhrsite.xshop.po",
+        "com.lhrsite.xshop.core"})
+@EnableJpaRepositories("com.lhrsite.xshop.repository")
+@MapperScan("com.lhrsite.xshop.mapper")
+@EntityScan("com.lhrsite.xshop.po")
 public class XshopAppApiApplication {
 
     public static void main(String[] args) {
