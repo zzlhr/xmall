01147aa735052808b095fd43afc8457310bbedb4
liuhaoran <2388399752@qq.com>
2019-09-17 20:07:48 +0800

    重写后台

---
+++ xshop-admin/src/main/java/com/lhrsite/xshopadmin/XshopAdminApplication.java
@@ -0,0 +1,13 @@
+package com.lhrsite.xshopadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XshopAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XshopAdminApplication.class, args);
+    }
+
+}
