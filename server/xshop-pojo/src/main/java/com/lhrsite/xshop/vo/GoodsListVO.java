0d5b1fbd83e1d53f7d36cba7add8a87daba02206
zzlhr <2388399752@qq.com>
2020-01-05 17:43:33 +0800

    修改商品列表视图

--- server/xshop-pojo/src/main/java/com/lhrsite/xshop/vo/GoodsListVO.java
+++ server/xshop-pojo/src/main/java/com/lhrsite/xshop/vo/GoodsListVO.java
@@ -1,47 +1,15 @@
 package com.lhrsite.xshop.vo;
 
 import lombok.Data;
-
-import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 @Data
 public class GoodsListVO {
 
     private String goodsId;
-    private String title;
-    private String describe;
-    private String content;
-
-
-    private BigDecimal originalPrice;
-
-    private BigDecimal salePrice;
-
-    private Integer saleStatus;
-
-    // 促销类型
-    private Integer saleType;
-
-    private String cover;
-
-    /**
-     * 状态
-     */
-    private Integer status;
-    /**
-     * 库存
-     */
-    private Integer stock;
-
-    /**
-     * 月销量
-     */
-    private Integer salesVolume;
-
-    private String deliveryPlace;
-
-    private BigDecimal despatchMoney;
+    private String goodsTitle;
+    private String goodsCover;
+    private String goodsStatus;
 
     private Integer updateUser;
 
@@ -51,11 +19,8 @@ public class GoodsListVO {
     private Timestamp createTime;
     private Timestamp updateTime;
 
-    private Integer clId;
-    private String clName;
+    private Integer categoryId;
 
-    private Integer clFid;
-    private String clFName;
-
+    private String categoryName;
 
 }
