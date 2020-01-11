06eee7c9954097ced6c0b159bc662287361c8bb2
haoranliu <2388399752@qq.com>
2020-01-11 09:29:44 +0800

    修复权限存储

--- xshop-admin/src/pages/UserLogin/utils/utils.js
+++ xshop-admin/src/pages/UserLogin/utils/utils.js
@@ -1,11 +1,16 @@
-import { parse } from 'qs';
+import {parse} from 'qs';
 
 export function getPageQuery() {
   return parse(window.location.href.split('?')[1]);
 }
+
 export function setAuthority(user) {
   // 保存user
   localStorage.setItem('user', JSON.stringify(user));
-  const proAuthority = [user.authCode];
+  
+  const proAuthority = [];
+  for (let i = 0; i < user.authGroups.length; i++) {
+    proAuthority[i] = user.authGroups[i].agCode;
+  }
   return localStorage.setItem('antd-pro-authority', JSON.stringify(proAuthority));
 }
